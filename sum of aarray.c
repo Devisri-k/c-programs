1dd5641953f18ba7d5d74587a7bca546874f2215
Devisri-k <125653937+Devisri-k@users.noreply.github.com>
2023-03-13 20:55:04 +0530

    Add files via upload

---
+++ sum of aarray.c
@@ -0,0 +1,14 @@
+#include <stdio.h>  
+int main()  
+{  
+     
+    int arr[4] = {1, 2, 3, 4};   
+    int sum = 0;  
+    int length = sizeof(arr)/sizeof(arr[0]);  
+    for (int i = 0; i < length; i++) 
+	{   
+       sum = sum + arr[i];  
+    }    
+    printf("Sum of all the elements of an array: %d", sum);  
+    return 0;  
+}
